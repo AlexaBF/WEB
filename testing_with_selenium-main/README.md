c41c421bd2695ac19c0235b4699666378507f5c2
AlexaBF <57883150+AlexaBF@users.noreply.github.com>
2022-06-21 17:06:20 -0500

    Add files via upload

---
+++ testing_with_selenium-main/README.md
@@ -0,0 +1,5 @@
+# testing_with_selenium
+you need a webdriver in my case i used chromedriver 101:
+https://chromedriver.chromium.org/downloads
+and the latest stable verison of selenium:
+https://www.selenium.dev/downloads/
---
+++ testing_with_selenium-main/selenium-test/src/App.java
@@ -0,0 +1,32 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver", "path to webdriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://mitec.itesm.mx/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        driver.findElement(By.id("Ecom_User_ID")).sendKeys("A01422793");
+        driver.findElement(By.id("Ecom_Password")).sendKeys("contraseña");
+        driver.findElement(By.id("submitButton")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElement(By.id("nffc")).sendKeys("codigodeverificacion");
+        driver.findElement(By.id("loginButton2")).click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        System.out.println(driver.findElement(By.className("user__name")).getText());
+        
+    }
+}
+
+//Ecom_User_ID
+//Ecom_Password
+//submitButton
+//div = user__name
